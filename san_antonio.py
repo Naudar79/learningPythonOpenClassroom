12627060ff311e7262e68ef125b690c2948635b7
Arnaud Want <76476235+Naudar79@users.noreply.github.com>
2021-07-26 10:52:41 +0200

    Update san_antonio.py

--- san_antonio.py
+++ san_antonio.py
@@ -1,19 +1,20 @@
 # -*- coding: utf8 -*-
 
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre"]
-
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre"]
 characters = ["alvin et les chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "kirikou"]
+def show_random_quote(my_list):
+    item = my_list[0]
+    return item
 
-# show random quote
-if user_answer == "B":
-    pass
-elif user_answer == "C":
-    print("C pas la bonne réponse ! et G pas d'humour, je C...")
-else:
-  
-    def show_random_quote(my_list):
-        quote = my_list[0]
-        print(quote)
+user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter. ")
 
-show_random_quote(quotes)
+
+while user_answer != "B":
+    print(show_random_quote(quotes))
+    user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter. ")
+
+for character in characters:
+    a_character = character.capitalize()
+    print(a_character)
+
+print(show_random_quote(quotes))
